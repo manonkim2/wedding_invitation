0915484a51e72f1172a9c1ec43271f4c05eed4cc
WY <kim.manon2@gmail.com>
2024-03-09 09:58:06 +0900

    font setting

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,7 @@ const cx = classNames.bind(styles)
 
 function App() {
   return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={cx('container')}>hi</div>
   )
 }
 
