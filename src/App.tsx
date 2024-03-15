23cd06c44fc24cdbafe8b41727fc60612f9b3bdf
Manon Kim <kim.manon2@gmail.com>
2024-03-15 23:15:35 +0900

    loading, error components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
+import FullScreenMessage from './components/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
+  const [wedding, setWedding] = useState(null)
+  const [loading, setloading] = useState(false)
+  const [error, setError] = useState(false)
+
   useEffect(() => {
-    return () => {}
+    setloading(true)
+
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('정보를 불러우지 못했습니다.')
+        }
+
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+      })
+      .catch((error) => {
+        console.log('에러발생', error)
+        setError(true)
+      })
+      .finally(() => {
+        setloading(false)
+      })
   }, [])
+
+  if (loading === false) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
 
   return <div className={cx('container')}>hi</div>
 }
