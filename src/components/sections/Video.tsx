8ee120748eb06a72d3e6fa39ec0db2117d0341fe
Manon Kim <kim.manon2@gmail.com>
2024-03-23 20:55:45 +0900

    font 변경

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,7 +5,13 @@ import styles from './Video.module.scss'
 const cx = classNames.bind(styles)
 
 const Video = () => {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video muted poster="/assets/thumbnail.png" controls>
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
 }
 
 export default Video
